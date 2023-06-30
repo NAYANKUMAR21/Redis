ff979cff9df976487b2603c834a5656e13c5fbf5
Nayan Kumar <naayaankumar@gmail.com>
2023-06-30 23:21:39 +0530

    secodnredis project

---
+++ SecondRedis/server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const JsonUrl = 'https://jsonplaceholder.typicode.com/todos/';
+let client = require('./init_Redis');
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const GetTodo = await client.get(`todo${id}`);
+
+    if (GetTodo) {
+      return res
+        .status(200)
+        .send({ message: 'Redis', GetTodo: JSON.parse(GetTodo) });
+    }
+    const fetchResult = await fetch(JsonUrl + id);
+    const data = await fetchResult.json();
+    console.log(data, 'here');
+    await client.set(`todo${id}`, JSON.stringify(data));
+
+    return res.status(200).send({ message: 'Normal Fetched', data });
+  } catch (er) {
+    return res.status(200).send({});
+  }
+});
+app.get('/', (req, res) => {
+  return res.status(200).send({ message: 'Home Page' });
+});
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
