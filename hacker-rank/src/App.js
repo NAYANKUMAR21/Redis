adc081e7124974758332f51da58c463568088b3f
Nayan Kumar <naayaankumar@gmail.com>
2023-07-16 01:25:28 +0530

    updated hacker

--- hacker-rank/src/App.js
+++ hacker-rank/src/App.js
@@ -7,32 +7,25 @@ const title = 'Sorting Articles';
 
 function App({ articles }) {
   const [Article, setArticleArray] = useState(articles);
-  const [count, setCount] = useState(0);
   const handleClickVotesDEscending = () => {
     console.log('inside 1');
     let x = Article.sort((a, b) => b.upvotes - a.upvotes);
     setArticleArray(x);
-    setCount(count + 1);
   };
 
   const handleCLickDateDescending = async () => {
     console.log('inside 2');
-    let y = await Article.sort((a, b) => new Date(b.date) - new Date(a.date));
+    let y = await Article.toSorted(
+      (a, b) => new Date(b.date) - new Date(a.date)
+    );
     setArticleArray(y);
-    setCount(count - 1);
   };
 
   useEffect(() => {
-    let x = Article.sort((a, b) => b.upvotes - a.upvotes);
+    let x = Article.toSorted((a, b) => b.upvotes - a.upvotes);
     setArticleArray(x);
-    setCount(count - 1);
   }, []);
-  const Inc = () => {
-    setCount(count + 1);
-  };
-  const Dec = () => {
-    setCount(count - 1);
-  };
+
   return (
     <div>
       <div>
--- index.js
+++ index.js
@@ -1,23 +1,2 @@
-//everything in javascript is an object  including function , an they are fist class objects
-const data = {
-  name: 'Nayan Kumar',
-  age: 21,
-  greet: function () {
-    setTimeout(function () {
-      console.log('hello', this.name);
-    });
-    console.log(this.name);
-  },
-};
-const laptop = {
-  brand: 'Dell',
-  getModel: function () {
-    const printBrand = () => {
-      console.log(this); //refers to parent object laptop
-    };
-    console.log(this); //also refers to main object laptop
-    printBrand();
-  },
-};
-data.greet();
-laptop.getModel();
+
+//let x = ans.length !== 0 ? ans.sort() : null;
